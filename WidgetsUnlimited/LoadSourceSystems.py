22f97e871ae0286c80fdf9f5f19ee1ddb76ed204
AlanHorowitz <alanhorowitz63@gmail.com>
2021-06-02 11:04:04 -0400

    refactoring

--- WidgetsUnlimited/LoadSourceSystems.py
+++ WidgetsUnlimited/LoadSourceSystems.py
@@ -4,34 +4,35 @@ from DataGeneration.Generator import Generator
 
 from domain.product import ProductTable
 from domain.store import StoreTable
-from domain.store_sales import STORE_SALES_TABLE
+from domain.store_sales import StoreSalesTable
 
 product_table = ProductTable()
 store_table = StoreTable()
+store_sales_table = StoreSalesTable()
+
+data_generator = Generator()
+e_commerce_system = eCommerceSystem(data_generator) 
+e_commerce_system.add_tables([product_table, store_table, store_sales_table])
 
 DailyOperations = [
         [   
             # day 1
-            TableUpdate(product_table, 5000, 0), 
-            TableUpdate(store_table, 40, 0)
+            TableUpdate(product_table, n_inserts=5000, n_updates=0), 
+            TableUpdate(store_table, n_inserts=40, n_updates=0)
         ],
         [
             # day 2
-            TableUpdate(product_table, 5, 50),
-            TableUpdate(store_table, 10, 20),
-            TableUpdate(STORE_SALES_TABLE, 50000, 0),
+            TableUpdate(product_table, n_inserts=5, n_updates=50),
+            TableUpdate(store_table, n_inserts=10, n_updates=20),
+            TableUpdate(store_sales_table, n_inserts=50000, n_updates=0),
         ],
         [
             # day 3
-            TableUpdate(product_table, 10, 30),
-            TableUpdate(store_table, 1, 0),
-            TableUpdate(STORE_SALES_TABLE, 50000, 0),
+            TableUpdate(product_table, n_inserts=10, n_updates=30),
+            TableUpdate(store_table, n_inserts=1, n_updates=0),
+            TableUpdate(store_sales_table, n_inserts=50000, n_updates=0),
         ],
     ]
-
-data_generator = Generator()
-eCommerceSys = eCommerceSystem(data_generator) 
-eCommerceSys.add_tables([PRODUCT_TABLE, STORE_TABLE, STORE_SALES_TABLE])
 
 # date = INIT_DATE
 for day in DailyOperations:
@@ -39,12 +40,7 @@ for day in DailyOperations:
         table_update.process()
         # bump time delta one dat
     
+e_commerce_system.remove_tables([product_table, store_table, store_sales_table])
+e_commerce_system.close()
 
-# gen.close()
-# ## what is my ETL going to do?
-
-# eCommerceOpSystem.remove_tables([table1, table2]) 
-# inStoreOpSystem.remove_tables([table1, table2])
-# inventoryOpSystem.remove_tables([table1, table2])
-
-
+data_generator.close()
