2f7676ea61a25a4e7b1dd33846a0ebdbea8bfc1d
Alan Horowitz <alanhorowitz63@gmail.com>
2021-07-29 21:40:32 -0400

    more modelling

--- WidgetsUnlimited/sourcesystems/loader.py
+++ WidgetsUnlimited/sourcesystems/loader.py
@@ -5,22 +5,25 @@ from .generator import DataGenerator
 from tables.product import ProductTable
 from tables.store import StoreTable
 from tables.store_sales import StoreSalesTable
+from tables.supplier import SupplierTable 
 
 class SourceSystemLoader:
     def load(self) -> None:
         product_table = ProductTable()
         store_table = StoreTable()
         store_sales_table = StoreSalesTable()
+        supplier_table = SupplierTable()
 
         data_generator = DataGenerator()
         e_commerce_system = eCommerceSystem(data_generator) 
-        e_commerce_system.add_tables([product_table, store_table, store_sales_table])
+        e_commerce_system.add_tables([product_table, store_table, store_sales_table, supplier_table])
 
         daily_operations = [
             [   
                 # day 1
                 TableUpdate(product_table, n_inserts=5000, n_updates=0), 
-                TableUpdate(store_table, n_inserts=40, n_updates=0)
+                TableUpdate(store_table, n_inserts=40, n_updates=0),
+                TableUpdate(supplier_table, n_inserts=40, n_updates=0)
             ],
             [
                 # day 2
