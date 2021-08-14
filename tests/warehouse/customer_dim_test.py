c1c49652e65478543157dc3e274bca45f62179c2
Alan Horowitz <alanhorowitz63@gmail.com>
2021-08-14 15:50:17 -0400

    test cleanup

--- tests/warehouse/customer_dim_test.py
+++ tests/warehouse/customer_dim_test.py
@@ -13,12 +13,14 @@ from .context import (
     parse_address
 )
 
-from datetime import date, datetime
 import pandas as pd
+from datetime import date, datetime
 from math import isnan
 
+# simplify test with simple representative structure
 cust_dim_cols = ['surrogate_key', 'customer_key', 'customer_name', 'customer_address_id', 'customer_address']
 
+# union of customer_id from two tables
 def test_get_customer_keys_incremental():
     customers = [[1, 'cust1'], [2, 'cust1'],
                  [3, 'cust1'], [4, 'cust1']]
@@ -32,10 +34,10 @@ def test_get_customer_keys_incremental():
     pd_cust = pd.DataFrame(customers, columns=['customer_id', 'name'])
     pd_cust_address = pd.DataFrame(customer_addresses, columns=['address_id', 'name', 'customer_id'])
 
-    ser = get_customer_keys_incremental(pd_cust, pd_cust_address)
-    assert sorted(ser.tolist()) == [1,2,3,4,6]
+    incremental_keys = get_customer_keys_incremental(pd_cust, pd_cust_address)
+    assert sorted(incremental_keys.tolist()) == [1,2,3,4,6]
 
-# all updates
+# only updates
 def test_get_new_keys_and_updates_1():
     
     cust_dim_data = [[1, 'cust_key_1', 'name_1', 'cust_addr_key_1', 'address_1'],
@@ -46,15 +48,15 @@ def test_get_new_keys_and_updates_1():
     incremental_keys = pd.Series(['cust_key_1', 'cust_key_2', 'cust_key_3'],
         name='customer_key')
 
-    new, update_df = get_new_keys_and_updates(incremental_keys, df_cust_dim) 
+    new_keys, updates_df = get_new_keys_and_updates(incremental_keys, df_cust_dim) 
 
-    assert sorted(new.to_list()) == []
-    assert update_df.size == 15     
-    update_df.set_index('customer_key', inplace=True)
-    assert update_df.at['cust_key_1', 'customer_name'] == 'name_1'
-    assert update_df.at['cust_key_3', 'customer_address'] == 'address_3'
+    assert sorted(new_keys.to_list()) == []
+    assert updates_df.size == 15     
+    updates_df.set_index('customer_key', inplace=True)
+    assert updates_df.at['cust_key_1', 'customer_name'] == 'name_1'
+    assert updates_df.at['cust_key_3', 'customer_address'] == 'address_3'
 
-# all new
+#only new
 def test_get_new_keys_and_updates_2():
     
     cust_dim_data = [[1, 'cust_key_1', 'name_1', 'cust_addr_key_1', 'address_1'],
@@ -64,10 +66,10 @@ def test_get_new_keys_and_updates_2():
     df_cust_dim = pd.DataFrame(cust_dim_data, columns=cust_dim_cols)
     incremental_keys = pd.Series(['cust_key_4', 'cust_key_5', 'cust_key_6'], name='customer_key')
 
-    new, update_df = get_new_keys_and_updates(incremental_keys, df_cust_dim) 
+    new_keys, updates_df = get_new_keys_and_updates(incremental_keys, df_cust_dim) 
 
-    assert sorted(new.to_list()) == ['cust_key_4', 'cust_key_5', 'cust_key_6']
-    assert update_df.size == 0  
+    assert sorted(new_keys.to_list()) == ['cust_key_4', 'cust_key_5', 'cust_key_6']
+    assert updates_df.size == 0  
     
 # mix of updates and new
 def test_get_new_keys_and_updates_3():
@@ -79,34 +81,35 @@ def test_get_new_keys_and_updates_3():
     df_cust_dim = pd.DataFrame(cust_dim_data, columns=cust_dim_cols)
     incremental_keys = pd.Series(['cust_key_2', 'cust_key_3', 'cust_key_4'], name='customer_key')
 
-    new, update_df = get_new_keys_and_updates(incremental_keys, df_cust_dim) 
+    new_keys, updates_df = get_new_keys_and_updates(incremental_keys, df_cust_dim) 
 
-    assert sorted(new.to_list()) == ['cust_key_4']
-    assert update_df.size == 10   # 5 * 2
-    update_df.set_index('customer_key', inplace=True)
-    assert update_df.at['cust_key_2', 'customer_name'] == 'name_2'
-    assert update_df.at['cust_key_3', 'customer_address'] == 'address_3'
+    assert sorted(new_keys.to_list()) == ['cust_key_4']
+    assert updates_df.size == 10   # 5 * 2
+    updates_df.set_index('customer_key', inplace=True)
+    assert updates_df.at['cust_key_2', 'customer_name'] == 'name_2'
+    assert updates_df.at['cust_key_3', 'customer_address'] == 'address_3'
 
-TEST_ADDRESS = "First Middle Last\n123 Snickersnack Lane\nBrooklyn, NY 11229"
+TEST_BILLING_ADDRESS = "First Middle Last\n123 Snickersnack Lane\nBrooklyn, NY 11229"
+TEST_SHIPPING_ADDRESS = "First Middle Last\n15 Jones Boulevard\nFair Lawn,NJ 07410"
+
 # test change
 def test_parse_address():
     
-    ser = parse_address(TEST_ADDRESS)
+    ser = parse_address(TEST_BILLING_ADDRESS)
     assert(ser['name'] == "First Middle Last")
     assert(ser['street_number'] == "123 Snickersnack Lane") 
     assert(ser['city'] == "Brooklyn")
     assert(ser['state'] == "NY") 
     assert(ser['zip'] == "11229")
 
-# Since this is for actual structured object, use full column list
+# test billing/shipping address parsing
 def test_build_new_dimension_1():
-
-    customer_table_cols = CustomerTable().get_column_names
-    customer_address_table_cols = CustomerAddressTable().get_column_names
 
     day = date(2020,10,10)
     dt_tm = datetime.now()
-    ta = TEST_ADDRESS
+    tba = TEST_BILLING_ADDRESS
+    tsa = TEST_SHIPPING_ADDRESS
+
     customer_stage_data  = \
                 {"customer_id" : [1,3,4,5],
                 "customer_name" : ['c1', 'c3', 'c4', 'c5'],
@@ -124,22 +127,17 @@ def test_build_new_dimension_1():
                 "customer_inserted_at" : [dt_tm, dt_tm, dt_tm, dt_tm],
                 "customer_updated_at" :  [dt_tm, dt_tm, dt_tm, dt_tm],
                 "batch_id" : [1,1,1,1] }
-
+    # new customers 3 & 5 have billing and shipping; customer 4, billing only,
     customer_stage_address_data = \
         {"customer_id" : [1,2,3,3,4,5,5],
          "customer_address_id" : [1,2,3,4,5,6,7],
-         "customer_address" : [ta] *7,
+         "customer_address" : [tsa, tba, tsa, tba, tba, tsa, tba],
          "customer_address_type" : ['S','B', 'S','B','B','S','B'],             
-         "customer_inserted_at" : [dt_tm, dt_tm, dt_tm, dt_tm, dt_tm, dt_tm, dt_tm],
-         "customer_updated_at" :  [dt_tm, dt_tm, dt_tm, dt_tm, dt_tm, dt_tm, dt_tm],
+         "customer_inserted_at" : [dt_tm] * 7,
+         "customer_updated_at" :  [dt_tm] * 7,
          "batch_id" : [1,1,1,1,1,1,1] }
 
     new_keys = pd.Series([3,4,5], name='customer_key')
-
-
-    # I need dataframes that have 
-    #  - customer_ids 1,3,4,5 (1 should be ignored)
-    #  - customer address 1,2, 2x3, 1x4, 2x5 (#4 has only billing address)
 
     customer_stage_df = pd.DataFrame(customer_stage_data)
     customer_address_stage_df = pd.DataFrame(customer_stage_address_data)
@@ -147,9 +145,12 @@ def test_build_new_dimension_1():
     inserts = build_new_dimension(new_keys, customer_stage_df, customer_address_stage_df)
     assert(inserts.shape[0] == 3)
     assert(inserts.at[3,'name'] == 'c3')
+    assert(inserts.at[3,'billing_state'] == 'NY')
+    assert(inserts.at[3,'shipping_state'] == 'NJ')
     assert(inserts.at[4,'billing_city'] == 'Brooklyn')
     assert(isnan(inserts.at[4,'shipping_city']))  #Format checking later
 
+# isolated referral_type parsing
 def test_build_new_dimension_2():
     customer_stage_data  = \
                 {"customer_id" : [1,2,3,4],
