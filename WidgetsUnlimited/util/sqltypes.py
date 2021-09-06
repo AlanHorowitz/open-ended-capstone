b810d60299c62ab0ec2ab90e64b88c7eb875d4b7
AlanHorowitz <alanhorowitz63@gmail.com>
2021-09-06 17:44:00 -0400

    documentation

--- WidgetsUnlimited/util/sqltypes.py
+++ WidgetsUnlimited/util/sqltypes.py
@@ -173,9 +173,9 @@ class Table:
             if len(self._update_columns) == 0:
                 raise Exception("Need at least one VARCHAR for update")
 
-            self._xrefDict: Dict[str, Table.XrefTableData] = Table._init_xref_dict(
-                self._columns
-            )
+            self._init_xref_dict()
+
+
 
     #
     #  Database table creation methods
@@ -230,17 +230,17 @@ class Table:
         these tables fit in memory for now.  Update the xrefDict with result set and count.
         """
         for table_name, table_data in self._xrefDict.items():
-            column_names = ",".join(table_data._column_list)
+            column_names = ",".join(table_data.column_list)
             cur.execute(f"SELECT {column_names} from {table_name};")
-            table_data._result_set = cur.fetchall()
-            table_data._num_rows = len(table_data._result_set)
+            table_data.result_set = cur.fetchall()
+            table_data.num_rows = len(table_data.result_set)
 
     def post_load(self) -> None:
         """Clear references to xref result set"""
         for table_data in self._xrefDict.values():
-            table_data._result_set = []
-            table_data._num_rows = 0
-            table_data._next_random_row = 0
+            table_data.result_set = []
+            table_data.num_rows = 0
+            table_data.next_random_row = 0
 
     def get_new_row(
         self,
@@ -284,41 +284,39 @@ class Table:
     class XrefTableData:
         """helper object for xref data"""
 
-        def __init__(
-                self, column_list=[], result_set=[], next_random_row=0, num_rows=0
-        ):
-            self._column_list = column_list
-            self._result_set = result_set
-            self._next_random_row = next_random_row
-            self._num_rows = num_rows
+        def __init__(self):
+            self.column_list = []
+            self.result_set = []
+            self.next_random_row = 0
+            self.num_rows = 0
 
-    @staticmethod
-    def _init_xref_dict(columns: List[Column]) -> Dict[str, XrefTableData]:
+    def _init_xref_dict(self) -> None:
         """ Create a dictionary mapping xref table names to helper object used to manage cross table lookups """
 
         xref_dict: Dict[str, Table.XrefTableData] = {}
-        for col in columns:
+
+        for col in self.get_columns():
             if col.is_xref():
                 xref_table, xref_column = col.get_xref_table(), col.get_xref_column()
                 if xref_table in xref_dict:
-                    xref_dict[xref_table]._column_list.append(xref_column)
+                    xref_dict[xref_table].column_list.append(xref_column)
                 else:
-                    xref_dict[xref_table] = Table.XrefTableData(
-                        column_list=[xref_column]
-                    )
-        return xref_dict
+                    xref_dict[xref_table] = Table.XrefTableData()
+                    xref_dict[xref_table].column_list.append(xref_column)
+
+        self._xrefDict = xref_dict
 
 
     def _setXrefTableRows(self):
         """Update the Xref dictionary with the current random rows for each table."""
         for table_data in self._xrefDict.values():
-            table_data._next_random_row = random.randint(0, table_data._num_rows - 1)
+            table_data.next_random_row = random.randint(0, table_data.num_rows - 1)
 
     def _getXrefValue(self, col: Column) -> str:
         """Return the column's value from the current random DictRow."""
 
-        row = self._xrefDict[col._xref_table]._next_random_row
-        value = self._xrefDict[col._xref_table]._result_set[row][col._xref_column]
+        row = self._xrefDict[col._xref_table].next_random_row
+        value = self._xrefDict[col._xref_table].result_set[row][col._xref_column]
 
         return value
 
